45379fdd0560c9fc2efa46effc73c7afaa433d91
Ibro <ibroabdraimov20@gmail.com>
2025-10-03 09:10:35 +0600

    sfdads

--- src/components/TestimonialsPage.jsx
+++ src/components/TestimonialsPage.jsx
@@ -204,10 +204,6 @@ const TestimonialsPage = () => {
               →
             </button>
           </div>
-
-          {/* Декоративные элементы */}
-          <div className="absolute -top-10 -left-10 w-20 h-20 bg-pink-200 rounded-full opacity-70"></div>
-          <div className="absolute -bottom-10 -right-10 w-20 h-20 bg-purple-200 rounded-full opacity-70"></div>
         </div>
 
         {/* Дополнительная статистика */}
